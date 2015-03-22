e918d88bb62bf3114f4371502de724a064c7d261
pfahey1 <pfahey182@gmail.com>
2015-03-22 19:02:21 -0400

    reddit clone to learn more about angluarjs. functional with basic authentication. more to be done

---
+++ app/scripts/app.js
@@ -0,0 +1,50 @@
+/* global app:true */
+/* exported app */
+/*'Firebase': false*/
+
+'use strict';
+
+var app = angular.module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ]);
+
+  app.config(function ($routeProvider) {
+	  $routeProvider
+	    .when('/', {
+	      templateUrl: 'views/posts.html',
+	      controller: 'PostsCtrl'
+	    }) 
+	    .when('/posts/:postId', {
+	        templateUrl: 'views/showpost.html',
+	        controller: 'PostViewCtrl'
+	      })
+	    .when('/register', {
+	    	  templateUrl: 'views/register.html',
+	    	  controller: 'AuthCtrl',
+	    	  resolve: {
+	    	    user: function(Auth) {
+	    	      return Auth.resolveUser();
+	    	    }
+	    	  }
+	    	})
+    	.when('/login', {
+    		  templateUrl: 'views/login.html',
+    		  controller: 'AuthCtrl',
+    		  resolve: {
+    		    user: function(Auth) {
+    		      return Auth.resolveUser();
+    		    }
+    		  }
+    		})
+	    .otherwise({
+        redirectTo: '/'
+      });
+  });
+  
+  app.constant('FIREBASE_URL', 'https://incandescent-heat-9180.firebaseIO.com/');
