e1757d8d432badd5fdfde157918a65a38f51c9de
pfahey1 <pfahey182@gmail.com>
2015-03-22 19:25:56 -0400

    added more athentication for users

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -5,9 +5,13 @@ app.controller('NavCtrl', function ($scope, $location, Post,Auth) {
   
   $scope.signedIn = Auth.signedIn;
   $scope.logout = Auth.logout;
+  $scope.user = Auth.user;
 
   $scope.submitPost = function () {
-	    Post.create($scope.post).then(function (ref) {
+	  $scope.post.creator = $scope.user.profile.username;
+	  $scope.post.creatorUID = $scope.user.uid;
+	  
+	  Post.create($scope.post).then(function (ref) {
 	      $location.path('/posts/' + ref.name());
 	      $scope.post = {url: 'http://', title: ''};
 	    });
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,8 +1,9 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope,$location, Post) {
+app.controller('PostsCtrl', function ($scope,$location, Post,Auth) {
 
 	 $scope.posts = Post.all;
+	 $scope.user = Auth.user;
 	  $scope.post = {url: 'http://'};
 
 
